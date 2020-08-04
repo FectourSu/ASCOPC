234026e833623f64ecffadd62502b4731d946a87
FectourSu <59044810+FectourSu@users.noreply.github.com>
2020-08-04 21:23:19 +0300

    Added header and section

---
+++ ASCOPC/Helpers/JSHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASCOPC.Helpers
+{
+    public static class JSHelper
+    {
+        public static async Task Scroll(this IJSRuntime jSRuntime, string id)
+        {
+            await jSRuntime.InvokeVoidAsync("blazorScrollToElementId", id);
+        }
+    }
+}
--- ASCOPC/wwwroot/js/scripts.js
+++ ASCOPC/wwwroot/js/scripts.js
@@ -1,1 +1,11 @@
 ﻿
+window.blazorScrollToElementId = (elementId) => {
+    var element = document.getElementById(elementId);
+    if (!element) {
+        console.warn('element was not found', elementId);
+        return false;
+    }
+
+    element.scrollIntoView({ block: "start", behavior: "smooth" });
+    return true;
+}
