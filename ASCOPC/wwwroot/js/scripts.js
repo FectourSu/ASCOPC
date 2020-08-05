37a5b72906689cd7a9ba8d5122667bf297931889
Dmitry <59044810+FectourSu@users.noreply.github.com>
2020-08-05 23:25:11 +0300

    Update scripts.js

--- ASCOPC/wwwroot/js/scripts.js
+++ ASCOPC/wwwroot/js/scripts.js
@@ -110,7 +110,11 @@ $(document).ready(function () {
 
 
 });
-
+$(document).ready(function () {
+if (window.matchMedia("(max-width: 450px)").matches) {
+new SmoothScroll(document, 120, 12);
+}
+});
 
 function SmoothScroll(target, speed, smooth) {
     if (target === document)
